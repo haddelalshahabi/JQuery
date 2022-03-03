afcac78ac6f5011aab47c5b94f412581249d70d5
haddelalshahabi <s362080@oslomet.no>
2022-03-03 02:16:04 +0100

    Initial commit

---
+++ src/main/java/com/example/introduksjontiljquery/IntroduksjonTilJQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.introduksjontiljquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroduksjonTilJQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntroduksjonTilJQueryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/introduksjontiljquery/IntroduksjonTilJQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.introduksjontiljquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroduksjonTilJQueryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
